a4df51cfb5175c0a7d02bea30de574b6a05ca3f3
Ismail Abdinur Muuse <ismailmuuse731@gmail.com>
2026-04-06 23:01:21 +0300

    my first python lesson

---
+++ main.py
@@ -0,0 +1,11 @@
+print("Hello, World!")
+print(" I will perform basic maths operations for you.")
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))                
+print("The sum of the two numbers is: ", num1 + num2)
+print("The difference of the two numbers is: ", num1 - num2)
+print("The product of the two numbers is: ", num1 * num2)
+if num2 != 0:
+    print("The quotient of the two numbers is: ", num1 / num2)
+else:    print("Cannot divide by zero.")
+    
