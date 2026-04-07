1048bb41b55db9fadd7e27c7b69ff932be065dd9
Ismail Abdinur Muuse <ismailmuuse731@gmail.com>
2026-04-07 10:26:01 +0300

    data types of python

---
+++ datatypes/datatpyes.py
@@ -0,0 +1,29 @@
+name = "ali"  // String data type
+
+
+age = 40 integer data type
+
+
+hieght = 1.87 flaot  data type
+
+
+is married = true boolean data type
+
+
+students = ["ali", "ahmed", "mohamed"] list data type
+
+
+
+
+person2 = {
+    "name": "ahmed",
+    "age": 30,                         // dictionary data type
+    "hieght": 1.75,
+    "is_married": False
+}
+
+names = ("ali", "ahmed", "mohamed")  // tuple data type
+
+
+
+
