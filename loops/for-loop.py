9bbaedec9f1a3c6321e332fe8406b848cb0ad803
Ismail Abdinur Muuse <ismailmuuse731@gmail.com>
2026-04-07 12:15:08 +0300

    created loops folder  and has five files

---
+++ loops/for-loop.py
@@ -0,0 +1,13 @@
+for i in range(29):
+    print(i)  # This will print numbers from 0 to 28, because the range function generates numbers up to, but not including, the specified number (29 in this case).
+
+
+
+    fruits = ["apple", "banana", "mango"]
+
+for fruit in fruits:
+    print(fruit)  # This will print each fruit in the list one by one. The loop iterates over each element in the 'fruits' list and prints it.
+
+    
+
+    
---
+++ loops/while-loop.py
@@ -0,0 +1,5 @@
+count = 0
+
+while count < 5:
+    print(count)
+    count += 1  # This will print numbers from 0 to 4. The loop continues as long as the condition (count < 5) is true. After each iteration, the count variable is incremented by 1.
