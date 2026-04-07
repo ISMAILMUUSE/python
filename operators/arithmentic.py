606d98676e6a2505d26cd977cb23d270c08d3224
Ismail Abdinur Muuse <ismailmuuse731@gmail.com>
2026-04-07 11:01:18 +0300

    added operators folder

---
+++ operators/arithmentic.py
@@ -0,0 +1,23 @@
+a =2
+b = 3
+print( a+b)     # addition operator
+
+c = 484
+d = 303
+print( c-d)     # subtraction operator
+
+e = 2323
+f = 2323
+print( e*f)     # multiplication operator
+
+g = 100
+h = 5
+print( g/h)     # division operator
+
+j = 10
+k = 3
+print( j//k)    # floor division operator
+
+l = 23
+m = 5
+print(l**m)     # modulus operator
